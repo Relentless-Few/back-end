01d236faadf750e421200f483c397894738e5c95
Nathan Loveless <nathansl2003@yahoo.com>
2020-08-07 13:48:04 -0400

    Connected back-end to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const schema = require("./api/schema/schema");
+const schema = require("./api/schema/mongoSchema");
 const expressGraphQL = graphqlHTTP;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config()
 
 const app = express();
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.wgs7j.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(bodyParser.json());
 
 app.use(
   "/graphql",
