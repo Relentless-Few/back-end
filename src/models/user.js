4aaf8ae1f7f253bba5fe04a49abf0481b27a0a61
Nathan Loveless <nathansl2003@yahoo.com>
2020-08-08 03:51:05 -0400

    Changed UserSchema, removed all database records

--- src/models/user.js
+++ src/models/user.js
@@ -4,14 +4,9 @@ import timestamps from 'mongoose-timestamp';
 import { composeWithMongoose } from 'graphql-compose-mongoose';
 
 export const UserSchema = new Schema({
-    uid: {type: String, trim: true, required: true},
+    uid: {type: String, required: true },
     displayName: {type: String, trim: true, required: true},
-    email: {type: String, lowercase: true, trim: true, unique: true, required: true},
-    apiKey: { type: String, trim: true, required: true},
-    organizationID: [{type: Number, trim: true}],
-    photoUrl: {type: String, trim: true},
-    phoneNumber: {type: String, trim: true},
-    lastLoginAt: {type: String, trim: true},
+    pat: {type: String, trim: true}
 },
 {
     collection: 'users'
